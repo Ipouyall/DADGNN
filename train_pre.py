eb81f853851e3b455db5405bed62dc7190fc08e1
pooyasadeghi <pouyasadeghi2012@gmail.com>
2023-09-02 15:26:24 +0330

    ...

--- train_pre.py
+++ train_pre.py
@@ -67,8 +67,8 @@ def test(model, dataset):
 def train(ngram, name, wd, bar, drop_out, num_hidden, num_layers, num_heads, k, alpha, dataset, is_cuda, edges=True):
 
     print('load data helper.')
-    path = '/DADGNN/data/' + dataset + '/' + dataset + '-vocab.txt'
-    with open(path,'r') as f:
+    path = 'data/' + dataset + '/' + dataset + '-vocab.txt'
+    with open(path, 'r') as f:
       vocab = f.read()
       vocab = vocab.split('\n')
     data_helper = DataHelper(dataset=dataset, mode='train', vocab=vocab)
